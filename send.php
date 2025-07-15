2716258956ad7809ac43409be07620f2255617cc
pradeep tiwari <pradeep.tiwari@nickelfox.com>
2025-07-15 23:34:05 +0530

    fix validation

--- send.php
+++ send.php
@@ -9,17 +9,12 @@ require_once __DIR__ . '/load_env.php';
 $mail = new PHPMailer(true);
 
 try {
-    $from      = $_POST['from'] ?? '';
-    $fromName  = $_POST['from_name'] ?? '';
     $toRaw     = $_POST['to'] ?? '';
     $ccRaw     = $_POST['cc'] ?? '';
     $bccRaw    = $_POST['bcc'] ?? '';
     $subject   = $_POST['subject'] ?? '';
     $message   = trim($_POST['message'] ?? '');
-
-    if (empty($from)) {
-        throw new Exception("You must provide sender email address.");
-    }
+    $provider  = $_POST['provider'] ?? 'gmail';
 
     if (empty($subject)) {
         throw new Exception("You must provide subject.");
@@ -29,27 +24,31 @@ try {
         throw new Exception("You must provide at least one recipient email address (To, CC, or BCC).");
     }
 
-    $mail->isSMTP();
-    $mail->Host       = 'smtp.gmail.com';
-    $mail->SMTPAuth   = true;
-    $mail->Username   = 'shivamtiwari1278@gmail.com';
-    $mail->Password   = 'csvxwylybwalxkzx';  // App password
-    $mail->SMTPSecure = 'tls';
-    $mail->Port       = 587;
+    switch ($provider) {
+        case 'gmail':
+            $mail->isSMTP();
+            $mail->Host       = getenv('SMTP_HOST');
+            $mail->SMTPAuth   = true;
+            $mail->Username   = getenv('SMTP_USERNAME');
+            $mail->Password   = getenv('SMTP_PASSWORD');
+            $mail->SMTPSecure = 'tls';
+            $mail->Port       = getenv('SMTP_PORT');
+            $mail->setFrom(getenv('SMTP_USERNAME'), getenv('SMTP_FROM_NAME'));
+            break;
 
-    $mail->isSMTP();
-    $mail->Host       = getenv('SMTP_HOST');
-    $mail->SMTPAuth   = true;
-    $mail->Username   = getenv('SMTP_USERNAME');
-    $mail->Password   = getenv('SMTP_PASSWORD');  // App password
-    $mail->SMTPSecure = 'tls';
-    $mail->Port       = getenv('SMTP_PORT');
-
-    // From Name & Email
-    $mail->setFrom(
-        !empty($from) ? $from : getenv('SMTP_USERNAME'),
-        !empty($fromName) ? $fromName : getenv('SMTP_FROM_NAME')
-    );
+        case 'sendgrid':
+            $mail->isSMTP();
+            $mail->Host       = 'smtp.sendgrid.net';
+            $mail->SMTPAuth   = true;
+            $mail->Username   = 'apikey';
+            $mail->Password   = getenv('SENDGRID_API_KEY');
+            $mail->SMTPSecure = 'tls';
+            $mail->Port       = 587;
+            $mail->setFrom(getenv('SENDGRID_FROM'), getenv('SENDGRID_FROM_NAME'));
+            break;
+        default:
+            throw new Exception("Unsupported email provider selected.");
+    }
 
     function addRecipients(PHPMailer $mail, string $list, string $type = 'to')
     {
